e48bf6e1f1481c55cca26f2d289ecce8954a26ef
Rahul-Gumgaonkar <rahulgumgaonkar4@gmail.com>
2024-02-11 22:47:00 +0530

    API

--- src/Components/NewsApp.js
+++ src/Components/NewsApp.js
@@ -7,7 +7,7 @@ function NewsApp() {
   const [newsList, setNewsList] = useState([]);
   const [query, setQuery] = useState("tesla");
   const queryInputRef = useRef(null);
-  const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2023-12-18&sortBy=publishedAt&apiKey=1c30091e98ae4eb78bbaaacfa1a4b227`;
+  const apiUrl = `https://newsapi.org/v2/everything?q=${query}&from=2024-01-11&sortBy=publishedAt&apiKey=1c30091e98ae4eb78bbaaacfa1a4b227`;
 
   useEffect(() => {
     fetchData();
